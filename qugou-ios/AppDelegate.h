c5a49ea32bfff2b5e2e6ed7c1b2d8808dda9aa60
yangqihua <904693433@qq.com>
2017-11-30 23:10:49 +0800

    init project 😁

---
+++ qugou-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qugou-ios
+//
+//  Created by yangqihua on 2017/11/30.
+//  Copyright © 2017年 yangqihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qugou-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qugou-ios
+//
+//  Created by yangqihua on 2017/11/30.
+//  Copyright © 2017年 yangqihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
